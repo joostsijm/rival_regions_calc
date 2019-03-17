175b8eb33d521a2a9d92d6d28540873f847ab3a5
JoostSijm <joostsijm@gmail.com>
2019-03-17 01:45:59 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="supremacy1914_wrapper",
-    version="0.1.1",
+    name="rival_regions_calc",
+    version="0.1.0",
     author="Joost Sijm",
     author_email="joostsijm@gmail.com",
-    description="Supremacy1914 API wrapper in Python",
+    description="Rival Regions calculations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joostsijm/supremacy1914_wrapper",
+    url="https://github.com/joostsijm/rival_regions_calc",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
